98c71de33b8639b8333a34854ebca362ea886bac
Steve Liddle <liddle@byu.edu>
2025-03-26 10:22:21 -0600

    Implemented a few CSS tweaks for the next/previous component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,6 @@
 /*----------------------------------------------------------------------
  *                      IMPORTS
  */
-import { Link } from "react-router-dom";
-import { ANIMATION_KEY_NEXT, ANIMATION_KEY_PREVIOUS } from "../Constants";
 import Breadcrumbs from "./Breadcrumbs";
 import "./Header.css";
 
@@ -20,14 +18,11 @@ import "./Header.css";
 export default function Header() {
     return (
         <header>
-            <Breadcrumbs />
-            <Link to="/1/106/12" state={{ animationKey: ANIMATION_KEY_PREVIOUS }}>
-                Go to Joshua 12
-            </Link>
             <div className="centerhead">
                 <div className="title">The Scriptures Mapped</div>
                 <div className="subtitle">By Stephen W. Liddle</div>
             </div>
+            <Breadcrumbs />
         </header>
     );
 }
